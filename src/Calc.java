4a27c1e53e23f3de2b63323fb5e3b9b1c0dd0ec3
yanyeef <yanyeef@nycstudents.net>
2022-11-30 20:44:04 -0500

    changes

---
+++ src/Calc.java
@@ -0,0 +1,99 @@
+public class Calc {
+    private double first;
+    private double second;
+    private String expression;
+    private String sign;
+    private double radian;
+
+    //constructor class
+    public Calc(double one, double two, String sign) {
+        this.first = one;
+        this.second = two;
+        this.sign = sign;
+    }
+
+    public Calc(double one, String sign){
+        this.first = one;
+        this.sign = sign;
+
+    }
+
+    public Calc(String expression){
+        this.expression = expression;
+    }
+
+    public double add(){
+        return first + second;
+    }
+
+    public double subtract() {
+        return first -second;
+    }
+
+    public double multiply() {
+        return first* second;
+    }
+
+    public double divide() {
+        return (double) first/ second;
+    }
+
+    public double radian(double first) {
+        this.radian =Math.toRadians(first);
+        return radian;
+    }
+
+    public double sin(double radian) {
+        return Math.sin(radian);
+    }
+
+    public double cos(double radian) {
+        return Math.cos(radian);
+    }
+
+    public double tan(double radian) {
+        return Math.tan(radian);
+    }
+
+    public double sqrt() {
+        return Math.sqrt(first);
+    }
+
+    public double pow() {
+        return Math.pow(first, second);
+    }
+
+    public String oneVal() {
+        double result =0;
+        if (sign.equals("sqrt")){
+            result = sqrt();
+        } else if (sign.equals("sin")){
+            result = sin(radian(first));
+        } else if (sign.equals("cos")){
+            result = cos(radian(first));
+        } else if(sign.equals("tan")){
+            result = tan(radian(first));
+        }
+        return sign +" " + first + " = "+ result;
+    }
+
+    public String twoVal() {
+        double result = 0;
+        if (sign.equals("+")) {
+            result = add();
+        } else if (sign.equals("-")) {
+            result = subtract();
+        } else if (sign.equals("*")) {
+            result = multiply();
+        } else if (sign.equals("/")) {
+            result = divide();
+        } else if (sign.equals("^")) {
+            result = pow();
+        }
+        return first + " "+ sign + " " + second + " = "+ result;
+    }
+
+
+
+
+}
---
+++ src/CalcRunner.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class CalcRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String expression = "";
+        System.out.print("Type in a value or sign: ");
+        String input = scan.nextLine();
+        expression += input + " ";
+        while (!(input.equals("="))) {
+            System.out.print("Type in a value or sign: ");
+            input = scan.nextLine();
+            expression += input + " ";
+        }
+
+        if (input.equals("=")) {
+            System.out.println(expression);
+            System.out.println("How much values did you input: ");
+            int numOfValues = scan.nextInt();
+            double result = 0;
+
+            if (numOfValues == 1) {
+                String single = expression.substring(expression.indexOf(" ") + 1, expression.indexOf("=") - 1);
+                double s = Double.parseDouble(single);
+                String math = expression.substring(0, expression.indexOf(" "));
+                Calc oneValue = new Calc(s, math);
+                System.out.println(oneValue.oneVal());
+
+            } else if (numOfValues == 2) {
+
+                String firstValue = expression.substring(0, expression.indexOf(" "));
+                double fV = Double.parseDouble(firstValue);
+                String secValue = expression.substring(expression.indexOf(" ") + 3, expression.indexOf("=") - 1);
+                double sV = Double.parseDouble(secValue);
+                String sign = expression.substring(expression.indexOf(" ") + 1, expression.indexOf(" ") + 2);
+
+                Calc twoNumbers = new Calc(fV, sV, sign);
+                System.out.println(twoNumbers.twoVal());
+            }
+
+        }
+    }
+}
